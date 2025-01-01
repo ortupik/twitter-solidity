32455e86bef2eb3baa3cba5d1533bf3328b96f9f
ortupik <chrisadriane.ca@gmail.com>
2025-01-01 12:27:48 +0300

    newest

---
+++ contracts/Twitter.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.26;
+
+contract Twitter {
+
+    uint16 constant MAX_TWEET_LENGTH = 280;
+    struct Tweet {
+        address author;
+        string content;
+        uint256 timestamp;
+        uint256 likes;
+    }
+
+    mapping(address => Tweet[]) private tweets;
+
+    function createTweet(string memory _tweet) public {
+        
+        require(bytes(_tweet).length <= MAX_TWEET_LENGTH, "Tweet is too long!!"); 
+        
+        Tweet memory newTweet =  Tweet({
+            author: msg.sender,
+            content: _tweet,
+            timestamp: block.timestamp,
+            likes: 0
+        });
+        tweets[msg.sender].push(newTweet);
+    }
+
+    function getTweet(address _owner, uint _i) public view returns (Tweet memory) {
+        return tweets[_owner][_i];
+    }
+
+    function getAllTweets(address _owner) public view returns (Tweet[] memory){
+        return tweets[_owner];
+    }
+
+}
